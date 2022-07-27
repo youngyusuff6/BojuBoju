7d249379d0b941861792175acd8d1797cfa213d1
AbdulHameed <youngyusuff006@gmail.com>
2022-07-27 18:48:06 +0100

    ________New

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Models\User;
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class HomeController extends Controller
 {
@@ -24,5 +29,33 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function showChangePasswordGet() {
+        return view('auth.passwords.change-password');
+    }
+
+    public function changePasswordPost(Request $request) {
+        if (!(Hash::check($request->get('current-password'), Auth::user()->password))) {
+            // The passwords matches
+            return redirect()->back()->with("error","Your current password does not matches with the password.");
+        }
+
+        if(strcmp($request->get('current-password'), $request->get('new-password')) == 0){
+            // Current password and new password same
+            return redirect()->back()->with("error","New Password cannot be same as your current password.");
+        }
+
+        $validatedData = $request->validate([
+            'current-password' => 'required',
+            'new-password' => 'required|string|min:8|confirmed',
+        ]);
+
+        //Change Password
+        $user = Auth::user();
+        $user->password = bcrypt($request->get('new-password'));
+        $user->save();
+
+        return redirect()->back()->with("success","Password successfully changed!");
     }
 }
---
+++ resources/views/settings/settings.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+@section('content')
+    
+    <div class="container bg-dark border ms-auto me-auto">
+        <div class="container text-center mt-3 ms-auto me-auto">
+            <a href="/changePassword" class="btn btn-danger">Change Password</a>
+        </div>
+        <div class="container text-center mt-3 ms-auto me-auto">
+            <a href="#" class="btn btn-danger">Delete Account</a>
+        </div>
+        <div class="container text-center mt-5 ms-auto me-auto">
+            <a href="/messages" class="btn btn-success">Go back</a>
+        </div>
+
+    </div>
+
+@endsection
