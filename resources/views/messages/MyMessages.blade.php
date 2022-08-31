464e7faf3d20c4bfc3f5147533ae6ecd27489e90
AbdulHameed <youngyusuff006@gmail.com>
2022-08-31 03:41:36 +0100

    Update

--- resources/views/messages/MyMessages.blade.php
+++ resources/views/messages/MyMessages.blade.php
@@ -3,17 +3,20 @@
 
 @section('styles')
     <link rel="stylesheet" href="{{url('vendor/css/mymessages.css')}}">
+<style>
+#body{
+  background-image: url('vendor/images/background-eye.gif');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size:cover;
+}
+</style>
 @endsection
 
-<body
-style=" background-image: url('');
-    background-repeat:no-repeat;
-    background-attachment:fixed;
-    background-size:cover;">
 
 @section('content')
 
-<div>
+<div id="body">
 
     <div>
         <a href="/messages" class="btn border-0 back-btn"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi back-icon bi-arrow-left-circle-fill" viewBox="0 0 16 16">
@@ -29,30 +32,30 @@ style=" background-image: url('');
         <div class="card-body">
           <p class="card-text">{{$message->message}}</p>
 
-          <div class="dropdown">
-  <button class="btn image-button dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-   Image
-  </button>
-  <ul class="dropdown-menu image-dropdown dropdown-menu-dark">
-    <li>
+          @if ($message->image)
   <p>
-              @if ($message->image)
-              <div class="card">
-               <a href="{{asset('storage/'.$message->image)}}" target="_blank"><img class="card-img-bottom" src="{{asset('storage/'.$message->image)}}" alt="message_img" style="width:100%"></a>
-                <div class="card-body">
-                </div>
+   <div class="dropdown card" style="width: 100%">
+      <button class="btn image-button dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+      View Image
+      </button>
+      <ul class="dropdown-menu image-dropdown dropdown-menu-dark card-body">
+        <div>
+                <div class="card">
+                <a href="{{asset('storage/'.$message->image)}}" target="_blank"><img class="card-img-bottom" src="{{asset('storage/'.$message->image)}}" alt="message_img" style="width:100%"></a>
+                  <div class="card-body">
+                  </div>
+              </div>
             </div>
+      </ul>
+   </div>
+  </p>
             @else
-            -No images attached
-              @endif
-            </p>
+            <p>
+            {{-- -No images attached --}}
+          </p>
+          @endif
 
-    </li>
-
-  </ul>
-</div>
-
-                <p>-BojuBoju [@ {{$message->created_at}}]</p>
+                <p class="mt-1 mb-1">-BojuBoju [@ {{$message->created_at}}]</p>
                 <div class="card-footer message-box bg-transparent">
               <a href="#" class="btn rounded mt-2 border-0  w-100 rounded-pill">Share response</a>
           </div>
@@ -67,8 +70,8 @@ style=" background-image: url('');
         <div class="card-header text-start">No recent messages</div>
       </div>
 @endif
-<div class="pagination justify-content-center">
+<div class="pagination justify-content-center mt-3">
     {{$messages->links()}}
 </div>
 </div></div>
-@endsection</body>
+@endsection
