9a4adb3c72d4f4206c720a095378c0b2c23d28a7
AbdulHameed <youngyusuff006@gmail.com>
2022-09-02 23:29:08 +0100

    Update on tables

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,7 +21,7 @@
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="username" class="col-md-4 login-label col-form-label text-md-end">{{ __('username') }}</label>
+                            <label for="username" class="col-md-4 login-label col-form-label text-md-end">{{ __('Username') }}</label>
 
                             <div class="col-md-6">
                                 <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
