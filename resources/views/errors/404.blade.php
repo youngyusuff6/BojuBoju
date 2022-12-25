7bddd62ccbf26a2e4cb33424b1eb87a103c974b8
priest-2105 <fawzybailey782@gmail.com>
2022-12-25 18:18:15 +0100

    error pages

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,42 @@
-@extends('errors::minimal')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bojuboju || 500</title>
+    <link rel="stylesheet" href="{{url('vendor/css/500.css')}}">
 
-@section('title', __('Not Found'))
-@section('code', '404')
-@section('message', __('Not Found'))
+
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="http://cdn.bootcss.com/toastr.js/latest/css/toastr.min.css">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{url('vendor/css/404.css')}}">
+
+    <!-- bootstrap icon  -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+
+
+
+</head>
+<body>
+
+
+<div class="body">
+
+<div class="error-message">
+<p>Page not found please return</p>
+<button class="btn btn-success" onclick="history.back()">Go Back</button>
+</div>
+
+</div>
+
+
+</body>
+</html>
--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,5 +1,45 @@
-@extends('errors::minimal')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bojuboju || 500</title>
+    <link rel="stylesheet" href="{{url('vendor/css/500.css')}}">
 
-@section('title', __('Server Error'))
-@section('code', '500')
-@section('message', __('Server Error'))
+
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="http://cdn.bootcss.com/toastr.js/latest/css/toastr.min.css">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/css/app.css')}}" rel="stylesheet">
+
+    <!-- bootstrap icon  -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+
+
+
+</head>
+<body>
+
+
+<div class="body">
+
+<div class="error-message">
+    
+<button class="btn btn-success" onclick="history.back()">Go Back</button>
+</div>
+
+</div>
+
+
+</body>
+</html>
+
+
+
